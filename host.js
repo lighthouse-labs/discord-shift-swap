83d31e9125ef255c028702a3768f9646c1337fd8
Jakemunro <jake.munro16@gmail.com>
2022-10-31 13:51:01 -0400

    Changed port on host.js

--- host.js
+++ host.js
@@ -4,4 +4,4 @@
 // app.listen(port, () => console.log(`Bot running on http://127.0.0.1:${port}`));
 
 const http = require("http");
-http.createServer((_, res) => res.end("ivon giveaways online")).listen(8080)
+http.createServer((_, res) => res.end("ivon giveaways online")).listen(4001)
