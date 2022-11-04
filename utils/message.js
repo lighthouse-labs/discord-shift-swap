b154a8e8c3a9293f255398e296434d9235b69f89
Jakemunro <jake.munro16@gmail.com>
2022-11-04 15:33:07 -0400

    Changed wording in message

--- utils/message.js
+++ utils/message.js
@@ -1,18 +1,18 @@
 const config = require('../config.json');
 module.exports = {
   giveaway:
-    (config.everyoneMention ? "@mentors\n\n" : "") +
-    "🎉 **A shift has been dropped!** 🎉",
+    (config.everyoneMention ? `<@&${993594724965421206}>\n\n` : "") +
+    "\n🎉 **SHIFT TRADE!** 🎉",
   giveawayEnded:
-    (config.everyoneMention ? "@mentors\n\n" : "") +
-    "❌❌ **The shift was claimed.** ❌❌",
+    (config.everyoneMention ? `<@&${993594724965421206}>\n\n` : "") +
+    "ℹ️ **SHIFT TRADE PERIOD ENDED.** ℹ️",
   drawing:  `Ends: **{timestamp}**`,
   color: "#012D3D",
-  inviteToParticipate: `React with 🎉 to participate!`,
-  winMessage: "Congratulations, {winners}! You've claimed **{this.prize}**!",  
+  inviteToParticipate: `React with ✋ to participate!`,
+  winMessage: "Congratulations, {winners}! You won the **{this.prize}** shift from {this.hostedBy}! Please make sure {this.hostedBy} updates the initials accordingly in the schedule.",  
   embedFooter: `{this.winnerCount} winner`,
-  noWinner: "Nobody claimed the shift, to drop the shift again, please start again.",
-  hostedBy: "Hosted by: {this.hostedBy}",
+  noWinner: "Hello {this.hostedBy}. \n This trade shift period has ended and nobody claimed your shift. If you'd like to try again, you can restart the command.",
+  hostedBy: "Hosted by: {this.hostedBy}.",
   winners: "winner",
   endedAt: "Ended at"
 }
