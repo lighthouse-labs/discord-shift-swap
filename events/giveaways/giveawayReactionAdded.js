ddf6c8843f37e95b27aae1e8385117d5d62c534b
Jakemunro <jake.munro16@gmail.com>
2022-11-10 11:07:08 -0500

    More punctuation

--- events/giveaways/giveawayReactionAdded.js
+++ events/giveaways/giveawayReactionAdded.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
 
 module.exports = {
-  async execute(trade, reactor, messageReaction,) {
+  async execute(trade, reactor, messageReaction) {
 
   let approved =  new MessageEmbed()
 
