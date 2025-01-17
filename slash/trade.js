092476111eb73a125e4c7ddd100c65e003cd5cfc
Jakemunro <jake.munro16@gmail.com>
2025-01-17 15:40:40 -0500

    Add option for TI trading

--- slash/trade.js
+++ slash/trade.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const messages = require("../utils/message");
+const messages = require("../utils/message")("trade");
 const ms = require("ms");
 module.exports = {
   name: "trade",
@@ -40,7 +40,7 @@ module.exports = {
     const channelId = allowedRoles[userRole.name];
     const tradeChannel = client.channels.cache.get(channelId);
 
-    const tradeDuration = ms(1000 * 60 * 15);
+    const tradeDuration = ms(1000 * 60 * 45);
     const tradeWinnerCount = 1;
     const tradePrize = interaction.options.getString("shift");
 
